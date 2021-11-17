8d3d6352d98b28ca5b61444a9644081ce112be38
mingg123 <spgb123spgb@gmail.com>
2021-11-18 00:39:42 +0900

    feat: add Programmers example

---
+++ minji/위장.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < clothes.length; i++) {
+            String cloth = clothes[i][0];
+            String type = clothes[i][1];
+            map.put(type, map.getOrDefault(type,0)+1);
+        }
+        answer = 1;
+        for(String key : map.keySet()) {
+            answer = (map.get(key) + 1) * answer;
+        }
+        return answer-1;
+    }
+}
