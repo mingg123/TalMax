6d60926092ff2ad5674f5014a582b226f7570522
mingg123 <spgb123spgb@gmail.com>
2021-11-19 18:26:52 +0900

    feat : add Programmers example 프린터

---
+++ minji/프린터.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<print> q = new LinkedList<>();
+        for(int i = 0; i < priorities.length; i++) {
+            q.add(new print(priorities[i], i));
+        }
+        int count = 0 ;
+        while(!q.isEmpty()) {
+            print top = q.poll();
+            boolean flag = false;
+            for(print pr : q) {
+              if(top.prior < pr.prior) {
+                  flag = true;
+              }
+            }
+            if(flag) {
+                q.add(top);
+            } else {
+                count ++;
+                if(top.idx == location) {
+                    answer = count;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+    class print{
+        int prior;
+        int idx;
+        public print(int prior, int idx) {
+            this.prior = prior;
+            this.idx = idx;
+        }
+    }
+}
