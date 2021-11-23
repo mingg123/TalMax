c18dc87dacb149e6f5ddba5bfbf2258fb832958b
mingg123 <spgb123spgb@gmail.com>
2021-11-23 23:08:35 +0900

    feat : add Programmers example 주식가격

---
+++ minji/주식가격.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i = 0; i < prices.length; i++) {
+            int target = prices[i];
+            int count = 1;
+            for(int j = i+1; j < prices.length-1; j++) {
+               if(target <= prices[j]) {
+                   count++;
+               } else {
+                   break;
+               }
+            }
+            answer[i] = count;
+        }
+        answer[prices.length-1] = 0;
+        return answer;
+    }
+}
