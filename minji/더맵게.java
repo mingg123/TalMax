5841c62e4dadd520271280cc747795f3c14f2299
mingg123 <spgb123spgb@gmail.com>
2021-11-23 23:27:24 +0900

    feat : add Programmers example 더 맵게

---
+++ minji/더맵게.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue <Integer> q = new PriorityQueue<>();
+        for(int i = 0 ; i < scoville.length; i++) {
+            q.add(scoville[i]);
+        }
+        while(q.peek() < K) {
+            if(q.size() >=2) {
+                int top = q.poll();
+                if(top >= K) {
+                    return answer;
+                } else {
+                    int second = q.poll();
+                    q.add(top + (second * 2));
+                    answer++;
+                }
+            } else {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
