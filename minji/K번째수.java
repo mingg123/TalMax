79e02ef87d6d5c700bc02226829bf108fadfd246
mingg123 <spgb123spgb@gmail.com>
2021-11-25 22:37:23 +0900

    feat : add programmers example K번째 수

---
+++ minji/K번째수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer =  new int[commands.length];
+        for(int i = 0; i < commands.length; i++) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            int start = commands[i][0]-1;
+            int end = commands[i][1]-1;
+            int idx = commands[i][2]-1;
+            for(int j = start; j <= end; j++) {
+                arr.add(array[j]);
+            }
+            Collections.sort(arr);
+            answer[i] = arr.get(idx);
+        }
+        return answer;
+    }
+}
