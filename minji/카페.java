1e8672c2a80c901618cd7a9788d2970f1995abb9
mingg123 <spgb123spgb@gmail.com>
2021-11-27 23:08:30 +0900

    feat : add Programmers example 카페

---
+++ minji/카페.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int total = yellow;
+        int row = 1;
+        int col = 1;
+        for(int i = 1; i <= total; i++) {
+            row = i;
+            if(total % i ==0) {
+                col = total / i;
+                if(row >= col) {
+                    if(row * 2 + col * 2 + 4 == brown) {
+                        row = row + 2;
+                        col = col + 2;
+                        break;
+                    }
+                }
+            }
+        }
+        answer[0] = row;
+        answer[1] = col;
+        return answer;
+    }
+}
