e0a9e8b9cb51dddd5ca20a97553e412020313f4f
mingg123 <spgb123spgb@gmail.com>
2021-11-28 16:08:36 +0900

    feat :add Programmers example 큰수 만들기

---
+++ minji/큰수만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class 큰수만들기 {
+    public String solution(String number, int k) {
+        int numLen = number.length() - k;
+        int index = 0;
+        int max = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numLen; i++) {
+            max = 0;
+            for (int j = index; j <= k + i; j++) {
+                // char to integer 인데 Character.getNumericValue(number.charAt(j))쓰면 실행 초과남.
+                if (max < (number.charAt(j) - '0')) {
+                    max = (number.charAt(j) - '0');
+                    index = j + 1;
+                }
+            }
+            sb.append(max);
+        }
+        return sb.toString();
+    }
+}
