d7b72c5048ccd22606921d4849b0f1c0500e1841
mingg123 <spgb123spgb@gmail.com>
2021-11-30 00:09:50 +0900

    feat : add Programmers example 섬연결하기

---
+++ minji/섬연결하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class 섬연결하기 {
+    public static int[] findParent;
+    public int find(int child) {
+        if(findParent[child] == child) {
+            return child;
+        } else {
+            return findParent[child] = find(findParent[child]);
+        }
+    }
+    public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int []o2) {
+                Integer a = o1[2];
+                Integer b = o2[2];
+                return a.compareTo(b);
+            }
+        });
+        findParent = new int[n];
+        for(int i = 0; i < n; i++) {
+            findParent[i] = i;
+        }
+        int answer = 0;
+        for(int i = 0; i< costs.length; i++) {
+            int firstIsland = find(costs[i][0]);
+            int SecondIsland = find(costs[i][1]);
+            if(firstIsland != SecondIsland) {
+                findParent[SecondIsland] = firstIsland;
+                answer += costs[i][2];
+            }
+        }
+     
+        return answer;
+    }
+}
