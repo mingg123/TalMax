6f40e86851cf448f21b2b87a5871162fee7db93c
mingg123 <spgb123spgb@gmail.com>
2021-12-04 14:30:12 +0900

    feat  : add Programmers example 입국 심사

---
+++ minji/입국심사.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class 입국심사 {
+    public long solution(int n, int[] times) {
+        long answer = Long.MAX_VALUE;
+        long min = 0;
+        long max = Long.MAX_VALUE;
+        long mid = 0;
+        while (min <= max) {
+            mid = (min + max) / 2;
+            long sum = 0;
+            for (int i = 0; i < times.length; i++) {
+                sum = sum + (mid / times[i]);
+                if (sum >= n)
+                    break;
+            }
+            if (sum >= n) {
+                max = mid - 1;
+                answer = Math.min(answer, mid);
+            } else {
+                min = mid + 1;
+            }
+        }
+        // System.out.println(mid);
+        return answer;
+    }
+}
