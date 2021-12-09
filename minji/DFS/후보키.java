57a0a32ff045fd03b28184fb01098dc4745488fc
mingg123 <spgb123spgb@gmail.com>
2021-12-09 19:07:39 +0900

    feat : add Programers example 행렬의곱셈

--- minji/DFS/후보키.java
+++ minji/DFS/후보키.java
@@ -1,55 +1,60 @@
+package DFS;
+
 import java.util.*;
+
 class 후보키 {
     public static int N;
-    public static boolean [] visit;
+    public static boolean[] visit;
     public static ArrayList<String> list;
     public static ArrayList<List<String>> result;
+
     public int solution(String[][] relation) {
         int answer = 0;
         visit = new boolean[relation[0].length];
         list = new ArrayList<>();
         result = new ArrayList<>();
         N = relation[0].length;
-        for(int i = 1; i <= relation[0].length; i++) {
+        for (int i = 1; i <= relation[0].length; i++) {
             dfs(visit, 0, i, "");
         }
-        for(int i = 0; i < list.size(); i++){
+        for (int i = 0; i < list.size(); i++) {
             HashSet<String> set = new HashSet<>();
             String token[] = list.get(i).split("");
-            for(int k = 0; k < relation.length; k++) {
+            for (int k = 0; k < relation.length; k++) {
                 String temp = "";
-                for(int j = 0; j < token.length; j++) {
+                for (int j = 0; j < token.length; j++) {
                     temp = temp + relation[k][Integer.parseInt(token[j])];
                 }
                 set.add(temp);
             }
-            if(set.size() == relation.length) {
-                List<String> temp = Arrays.asList(list.get(i).split("")); 
-				boolean flag = true;
-				for(int j = 0; j < result.size(); j++) {
-					if(temp.containsAll(result.get(j))) { 
-						flag = false;
-						break;
-					}
-				}
-				if(flag) { 
-					result.add(temp);
-				}
-			}
-		
-         }
+            if (set.size() == relation.length) {
+                List<String> temp = Arrays.asList(list.get(i).split(""));
+                boolean flag = true;
+                for (int j = 0; j < result.size(); j++) {
+                    if (temp.containsAll(result.get(j))) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    result.add(temp);
+                }
+            }
+
+        }
         return result.size();
     }
+
     public static void dfs(boolean[] visit, int idx, int r, String token) {
-        if(r == 0) {
+        if (r == 0) {
             // System.out.println(token);
             list.add(token);
             return;
         }
-        for(int i = idx; i < N; i++) {
-            if(!visit[i]) {
+        for (int i = idx; i < N; i++) {
+            if (!visit[i]) {
                 visit[i] = true;
-                dfs(visit, i, r-1, token+ String.valueOf(i));
+                dfs(visit, i, r - 1, token + String.valueOf(i));
                 visit[i] = false;
             }
         }
--- minji/최소공배수/N개의최소공배수.java
+++ minji/최소공배수/N개의최소공배수.java
@@ -1,20 +1,25 @@
+package 최소공배수;
+
 import java.util.*;
+
 class N개의최소공배수 {
     public int solution(int[] arr) {
         int answer = 0;
         Arrays.sort(arr);
-        int ans = 1; 
-        for(int i = 0; i < arr.length; i++) {
+        int ans = 1;
+        for (int i = 0; i < arr.length; i++) {
             ans = sol(ans, arr[i]);
         }
         return ans;
     }
+
     public static int sol(int a, int b) {
-        return a * b / gcd(a,b);
+        return a * b / gcd(a, b);
     }
+
     public static int gcd(int a, int b) {
-        while (b!= 0) {
-            int r = a%b;
+        while (b != 0) {
+            int r = a % b;
             a = b;
             b = r;
         }
